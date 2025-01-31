20f015678ee498c5e3e76a2cb9d9ab00a4d41542
Wallace Wambulwa <wallacewambulwa@gmail.com>
2025-01-31 16:40:00 +0300

    nabvar development

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,55 +1,36 @@
-import { useState } from "react";
-import { FaShoppingCart, FaUser, FaBars, FaTimes } from "react-icons/fa";
+
 import "./styles/navbar.css";
+import React from "react";
+import { FaSearch, FaUser, FaQuestionCircle, FaShoppingCart } from "react-icons/fa";
 
 const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
   return (
     <nav className="navbar">
       {/* Logo */}
-      <div className="logo">The Conscious Cart</div>
+      <div className="logo">ConsciousCart<span className="star">★</span></div>
 
       {/* Search Bar */}
       <div className="search-container">
-        <input
-          type="text"
-          placeholder="Search for healthy foods..."
-          className="search-input"
-        />
+        <FaSearch className="search-icon" />
+        <input type="text" placeholder="Search products, brands and categories" className="search-input" />
         <button className="search-button">Search</button>
       </div>
 
-      {/* Icons & Mobile Menu Button */}
+      {/* Icons Section */}
       <div className="icon-container">
-        <button className="icon-button">
-          <FaShoppingCart />
-        </button>
-        <button className="icon-button">
+        <div className="icon">
           <FaUser />
-        </button>
-        <button
-          className="menu-button"
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
-          {menuOpen ? <FaTimes /> : <FaBars />}
-        </button>
-      </div>
-
-      {/* Mobile Menu */}
-      {menuOpen && (
-        <div className="mobile-menu">
-          <input
-            type="text"
-            placeholder="Search for healthy foods..."
-            className="mobile-search-input"
-          />
-          <button className="mobile-search-button">Search</button>
-          <a href="#" className="menu-item">Groceries</a>
-          <a href="#" className="menu-item">Fruits</a>
-          <a href="#" className="menu-item">Drinks</a>
+          <span>Account</span>
         </div>
-      )}
+        <div className="icon">
+          <FaQuestionCircle />
+          <span>Help</span>
+        </div>
+        <div className="icon">
+          <FaShoppingCart />
+          <span>Cart</span>
+        </div>
+      </div>
     </nav>
   );
 };
